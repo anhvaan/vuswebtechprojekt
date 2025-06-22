c2621159da92c98f1d4ca26720769bad4dc1c0e7
anhvaan <ThiAnh.Vu@Student.HTW-Berlin.de>
2025-06-22 20:45:35 +0200

    Add Recipe Entity Klasse

---
+++ src/main/java/com/anhvaan/webtech_projekt/Recipe.java
@@ -0,0 +1,82 @@
+package com.anhvaan.webtech_projekt;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String ingredients;
+
+    private String instructions;
+
+    public Recipe() {}
+
+    public Recipe(String title, String description, String ingredients, String instructions) {
+        this.title = title;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", ingredients='" + ingredients + '\'' +
+                ", instructions='" + instructions + '\'' +
+                '}';
+    }
+}
