f3d1cdc27f3b680d84207ea961caf35bf8748598
anhvaan <ThiAnh.Vu@Student.HTW-Berlin.de>
2025-06-28 00:41:43 +0200

    update RecipeRepository.java

--- src/main/java/com/anhvaan/webtech_projekt/RecipeRepository.java
+++ src/main/java/com/anhvaan/webtech_projekt/RecipeRepository.java
@@ -1,7 +1,14 @@
 package com.anhvaan.webtech_projekt;
 
-import org.springframework.data.repository.CrudRepository;
+import com.anhvaan.webtech_projekt.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface RecipeRepository extends CrudRepository<Recipe, Long> { }
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    List<Recipe> findByUserId(String userId);
+
+    List<Recipe> findByUserIdAndIsFavoriteTrue(String userId);
+}
